7c7ff58cb61bc15333517781749a83a6afc5bb87
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-07-30 16:21:44 +0430

    Add files via upload

---
+++ saf 2 stack/saf 2 stack/Class1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saf_2_stack
+{
+    internal class Class1
+    {
+        int[] array1;
+        int[] array2;
+        int top1 = -1;
+        int top2 = -1;
+        int arraylength;
+
+        public Class1(int len)
+        {
+            arraylength = len;
+            array1 = new int[arraylength];
+            array2 = new int[arraylength];
+        }
+
+        public bool isfull()
+        {
+            if (top1 >= array1.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isempty()
+        {
+            if (top1 == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Enqueue(int val)
+        {
+            if (!isfull())
+            {
+                top1++;
+                array1[top1] = val;
+            }
+            else
+            {
+                Console.WriteLine("table is full");
+            }
+        }
+        //public void dequeue()
+        //{
+        //    if (!isempty())
+        //    {
+        //        for (int i = top1; i == 0; i--)
+        //        {
+        //            top2+=3;
+        //            array2[top2] = array1[i];
+        //            top1--;
+        //        }
+        //        //int x = array2[top2];
+        //        //Console.WriteLine(x);
+        //        top2--;
+        //        for (int j = top2; j == 0; j--)
+        //        {
+                    
+        //            array1[top1] = array2[j];
+        //        }
+               
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("table is empty");
+        //    }
+
+        //}
+        public void Deqeueu()
+        {
+            if (!isempty())
+            {
+                for (int i = top1; i > -1; i--)
+                {
+                    top2++;
+                    array2[top2] = array1[i];
+                }
+                top2 --;
+                for (int j = top2; j > -1; j--)
+                {
+                    top1 = 0;
+                    array1[top1]= array2[j];
+                    top1++;
+                }
+                 
+
+            }
+
+        }
+
+        public void printnum()
+        {
+            for(int i = 0; i <= top1; i++)
+                Console.WriteLine(array1[i]);
+            
+        }
+        
+
+
+
+
+    }
+}
---
+++ saf 2 stack/saf 2 stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saf_2_stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1(3);
+
+            a.Enqueue(1);
+            a.Enqueue(2);
+            a.Enqueue(3);
+            a.Deqeueu();
+            a.Enqueue(4);
+            a.printnum();
+
+            //a.dequeue();
+            //a.printnum();
+            //a.dequeue();
+            //a.dequeue();
+            //a.dequeue();
+
+            Console.ReadKey();
+        }
+    }
+}
